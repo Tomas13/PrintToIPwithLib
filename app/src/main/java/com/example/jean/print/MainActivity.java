cd898f438f79947e2fe45674848eeaa3b24d191c
Tomas13 <tomas.mapoter@gmail.com>
2017-05-26 09:00:57 +0600

    ilc

--- app/src/main/java/com/example/jean/print/MainActivity.java
+++ app/src/main/java/com/example/jean/print/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.jean.print;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintJob;
@@ -11,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.maxxton.printer.PrintDocument;
@@ -20,18 +23,26 @@ import com.maxxton.printer.PrintProtocol;
 import com.maxxton.printer.Printer;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import it.custom.printer.api.android.CustomAndroidAPI;
+import it.custom.printer.api.android.CustomException;
+import it.custom.printer.api.android.CustomPrinter;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LogMainActi";
     @BindView(R.id.webview)
     WebView mWebView;
+    InetAddress iAddress;
 
     private WebView webView;
+    private CustomPrinter prnDevice = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +50,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        doWebViewPrint();
+        ipPrintService = new ipPrintFile(this, mHandler);
+        mConversationArrayAdapter = new ArrayAdapter<String>(this, R.id.remote_device);
 
-        try{
+        connectToDevice("10.62.10.153");
+        try {
 
             new RetrieveFeedTask().execute();
-        }catch (Exception e){
+        } catch (Exception e) {
             write(e.getMessage());
         }
     }
-    
-    private void write(String msg){
+
+    private void write(String msg) {
         runOnUiThread(() -> {
             Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
         });
@@ -78,12 +91,29 @@ public class MainActivity extends AppCompatActivity {
             // TODO: check this.exception
             // TODO: do something with the feed
 
-            if (this.exception != null){
+            try {
+                String[] ethPrin =
+                        CustomAndroidAPI.EnumEthernetDevices(5000, getApplicationContext());
 
-                runOnUiThread(() -> {
-                    Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
-                });
+//                Log.d("Jean", ethPrin.length + "");
+                prnDevice = new CustomAndroidAPI()
+//                        .getPrinterDriverETH("10.62.10.153");
+                        .getPrinterDriverETH(iAddress.getHostAddress());
+                prnDevice.feed(1);
+                prnDevice.printText("Test test");
+                prnDevice.printText("Сактандыру\t\t\t\t\t Авиа");
+                prnDevice.printText("Сдать\t\t\t\t\t");
+                prnDevice.printText("Куда Участок МЖД и ПК г.Астана [220081]");
+                prnDevice.printText("Куда Участок сортировки письменной [220097]");
+
+
+                prnDevice.printText("\t\tВес 10.750 кг");
+                prnDevice.printText("Пломба № 1987621");
+
+            } catch (CustomException e) {
+                e.printStackTrace();
             }
+
         }
     }
 
@@ -91,21 +121,48 @@ public class MainActivity extends AppCompatActivity {
     private void shit() {
         try {
 
-            Printer printer = new Printer("192.168.204.16");
+
+            Printer printer = new Printer("10.62.10.153");
             PrintDocument document = new PrintDocument("document astana Zhangali");
-            document.insert("Hello kazpost");
+            document.insert("Сактандыру\t\t\t\t\t Авиа");
+            document.insert("Сдать\t\t\t\t\t");
+            document.insert("Куда Участок МЖД и ПК г.Астана [220081]");
+            document.insert("Куда Участок сортировки письменной [220097]");
+
+//            prnDevice.printText("sdfdf");
+
+            document.insert("\t\tВес 10.750 кг");
+            document.insert("Пломба № 1987621");
+
 
             printer.print(document, PrintProtocol.RAW);
 
-/*
-            Socket sock = new Socket("192.168.204.16", 9100);
+
+            String[] ethPrin =
+                    CustomAndroidAPI.EnumEthernetDevices(5000, getApplicationContext());
+
+//            Log.d("Jean", ethPrin.length + "");
+            prnDevice = new CustomAndroidAPI()
+                    .getPrinterDriverETH(iAddress.getHostAddress());
+            prnDevice.feed(1);
+            prnDevice.printText("Test test");
+            prnDevice.printText("Сактандыру\t\t\t\t\t Авиа");
+            prnDevice.printText("Сдать\t\t\t\t\t");
+            prnDevice.printText("Куда Участок МЖД и ПК г.Астана [220081]");
+            prnDevice.printText("Куда Участок сортировки письменной [220097]");
+
+
+            prnDevice.printText("\t\tВес 10.750 кг");
+            prnDevice.printText("Пломба № 1987621");
+
+
+            Socket sock = new Socket("10.62.10.153", 9100);
             PrintWriter oStream = new PrintWriter(sock.getOutputStream());
             oStream.println("HI,test from Android Device");
             Log.d("He", "here");
             oStream.println("\n\n\n\f");
             oStream.close();
             sock.close();
-*/
         } catch (Exception e) {
 
 
@@ -114,6 +171,142 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void connectToDevice(String remote) {
+        //String remote = mRemoteDevice.getText().toString();
+        if (remote.length() == 0)
+            return;
+        //disconnect first
+/*
+        if (ipPrintService.getState() == ipPrintFile.STATE_CONNECTED) {
+            ipPrintService.stop();
+            setConnectState(ipPrintFile.STATE_DISCONNECTED);
+            //return;
+        }
+*/
+
+        try {
+            iAddress = InetAddress.getByName(remote);
+        }catch (UnknownHostException e){
+            Toast.makeText(this, "Invalid IP host", Toast.LENGTH_SHORT)
+                    .show();
+            remote=null;
+        }
+
+        if (remote != null) {
+            Log.d("Main", "connecting to " + remote);
+            ipPrintService.connect(remote);
+        } else {
+            Log.d("Main", "unknown remote device!");
+        }
+    }
+
+    ipPrintFile ipPrintService = null;
+    void setConnectState(Integer iState) {
+        switch (iState) {
+            case ipPrintFile.STATE_CONNECTED:
+//                updateConnectButton(true);
+                break;
+            case ipPrintFile.STATE_DISCONNECTED:
+//                updateConnectButton(false);
+                break;
+            case ipPrintFile.STATE_CONNECTING:
+                Log.d("Main", "connecting...");
+                break;
+            case ipPrintFile.STATE_LISTEN:
+                Log.d("Main", "listening...");
+                break;
+            case ipPrintFile.STATE_IDLE:
+                Log.d("Main", "state none");
+                break;
+            default:
+                Log.d("Main", "unknown state var " + iState.toString());
+        }
+    }
+    private static final boolean D = true;
+    private String mConnectedDeviceName = null;
+    private ArrayAdapter<String> mConversationArrayAdapter;
+
+    // The Handler that gets information back from the ipPrintService
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mConnectedDeviceName = msg.getData().getString(msgTypes.DEVICE_NAME);
+
+            switch (msg.what) {
+                case msgTypes.MESSAGE_STATE_CHANGE:
+                    Bundle bundle = msg.getData();
+                    int status = bundle.getInt("state");
+                    if (D)
+                        Log.i(TAG, "handleMessage: MESSAGE_STATE_CHANGE: " + msg.arg1);  //arg1 was not used! by ipprintFile
+                    setConnectState(msg.arg1);
+                    switch (msg.arg1) {
+                        case ipPrintFile.STATE_CONNECTED:
+                            Log.d("Main", "connected to: " + mConnectedDeviceName);
+                            mConversationArrayAdapter.clear();
+                            Log.i(TAG, "handleMessage: STATE_CONNECTED: " + mConnectedDeviceName);
+                            break;
+                        case ipPrintFile.STATE_CONNECTING:
+                            Log.d("Main", "connecting...");
+                            Log.i(TAG, "handleMessage: STATE_CONNECTING: " + mConnectedDeviceName);
+                            break;
+                        case ipPrintFile.STATE_LISTEN:
+                            Log.d("Main", "connection ready");
+                            Log.i(TAG, "handleMessage: STATE_LISTEN");
+                            break;
+                        case ipPrintFile.STATE_IDLE:
+                            Log.d("Main", "STATE_NONE");
+                            Log.i(TAG, "handleMessage: STATE_NONE: not connected");
+                            break;
+                        case ipPrintFile.STATE_DISCONNECTED:
+                            Log.d("Main", "disconnected");
+                            Log.i(TAG, "handleMessage: STATE_DISCONNECTED");
+                            break;
+                    }
+                    break;
+                case msgTypes.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case msgTypes.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    Log.d("Main", "recv>>>" + readMessage);
+                    break;
+                case msgTypes.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(msgTypes.DEVICE_NAME);
+                    //Toast.makeText(getApplicationContext(), "Connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();(mConnectedDeviceName, "Connected");
+                    Log.i(TAG, "handleMessage: CONNECTED TO: " + msg.getData().getString(msgTypes.DEVICE_NAME));
+                    //printESCP();
+//                    updateConnectButton(false);
+
+                    break;
+                case msgTypes.MESSAGE_TOAST:
+//                    Toast toast = Toast.makeText(getApplicationContext(), msg.getData().getString(msgTypes.TOAST), Toast.LENGTH_SHORT);//.show();
+//                    toast.setGravity(Gravity.CENTER,0,0);
+//                    toast.show();
+//                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();(msg.getData().getString(msgTypes.TOAST));
+                    Log.i(TAG, "handleMessage: TOAST: " + msg.getData().getString(msgTypes.TOAST));
+                    Log.d("Main", msg.getData().getString(msgTypes.TOAST));
+                    break;
+                case msgTypes.MESSAGE_INFO:
+                    Log.d("Main", msg.getData().getString(msgTypes.INFO));
+                    //mLog.append(msg.getData().getString(msgTypes.INFO));
+                    //mLog.refreshDrawableState();
+                    String s = msg.getData().getString(msgTypes.INFO);
+                    if (s.length() == 0)
+                        s = String.format("int: %i" + msg.getData().getInt(msgTypes.INFO));
+                    Log.i(TAG, "handleMessage: INFO: " + s);
+                    break;
+            }
+        }
+    };
 
     private void doWebViewPrint() {
         // Create a WebView object specifically for printing
